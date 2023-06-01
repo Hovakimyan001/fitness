da53611cba08136be350c410900abb4564a4050c
Hovakimyan001 <qristian.hovakimyan@bk.ru>
2023-06-01 12:07:01 +0400

    fitness

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SetcionTwo from './component/SetcionTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <SetcionTwo />
+    </div>
+  );
+}
+
+export default App;
